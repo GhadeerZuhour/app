c3d9a5ab288299582dcaf5a111667bbaf34642c6
Ghadeer Zuhour <ghadeer.zuhour@gmail.com>
2026-01-22 13:02:07 +0200

    up

--- resources/views/livewire/admin/businesses/create.blade.php
+++ resources/views/livewire/admin/businesses/create.blade.php
@@ -5,22 +5,34 @@
         <div class="col-md-6">
             <input class="form-control" placeholder="Owner name"
                 wire:model.defer="owner_name">
+            @error('owner_name')
+                <div class="text-danger small">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="col-md-6">
             <input class="form-control" placeholder="Owner email"
                 wire:model.defer="owner_email">
+            @error('owner_email')
+                <div class="text-danger small">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="col-md-6">
             <input type="password" class="form-control"
                 placeholder="Password"
                 wire:model.defer="password">
+            @error('password')
+                <div class="text-danger small">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="col-md-6">
             <input class="form-control" placeholder="Business name"
                 wire:model.defer="business_name">
+            @error('business_name')
+                <div class="text-danger small">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="col-md-6">
@@ -29,11 +41,17 @@
                 <option value="monthly">Monthly</option>
                 <option value="yearly">Yearly</option>
             </select>
+            @error('subscription_period')
+                <div class="text-danger small">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="col-md-6">
             <input type="date" class="form-control"
                 wire:model.defer="subscription_ends_at">
+            @error('subscription_ends_at')
+                <div class="text-danger small">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="col-12">
