180dd30217c48af80f02d570e09509b5e1d24c22
GhadeerZuhour <ghadeer.zuhour@gmail.com>
2026-08-09 00:12:11 +0300

    Polish English demo labels

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -3,17 +3,26 @@
 return [
     'dashboard' => 'Dashboard',
     'accounts' => 'Accounts',
-    'entries' => 'Entries',
-    'create_entry' => 'Create Entry',
+    'entries' => 'Transactions',
+    'transactions' => 'Transactions',
+    'create_entry' => 'Add Transaction',
+    'support' => 'Support',
+    'tenants' => 'Companies',
+    'add_tenant' => 'Add Company',
 
     'save' => 'Save',
     'cancel' => 'Cancel',
     'edit' => 'Edit',
     'delete' => 'Delete',
     'actions' => 'Actions',
+    'logout' => 'Sign out',
 
     'date' => 'Date',
     'amount' => 'Amount',
     'account' => 'Account',
     'payment_method' => 'Payment Method',
+
+    'brand_tagline' => 'Know your cash before it surprises you.',
+    'workspace' => 'Cash-flow workspace',
+    'language' => 'العربية',
 ];
